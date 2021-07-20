f1b02c22312644e78374eb80bdde5a26be5c6c44
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-07-20 22:32:47 +0200

    feat: add form components to root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+
+import { MatDialog } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'myvhs-client-a';
+
+  constructor(public dialog: MatDialog) {}
+
+  // Open user registration dialog when "Sign up" button is clicked.
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      // Set the width of the dialog window.
+      width: '280px',
+    });
+  }
+
+  // Open user registration dialog when "Log in" button is clicked.
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      // Set the width of the dialog window.
+      width: '280px',
+    });
+  }
 }
