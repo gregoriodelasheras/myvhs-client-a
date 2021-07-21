6b76b16de037049e89262999333b202b016f662c
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-07-21 19:45:40 +0200

    feat: create movie-card component - Store the movies returned by the API call. - Fetch all movies from FetchApiDataService with getAllMovies function. - Display each movie with title and image on a card.

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss'],
+})
+export class MovieCardComponent implements OnInit {
+  // Store the movies returned by the API call.
+  movies: any[] = [];
+  constructor(public fetchApiData: FetchApiDataService) {}
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  // Fetch all movies from FetchApiDataService with getAllMovies function.
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      return this.movies;
+    });
+  }
+}
