f495c627dcfaab9148d6295b986c882f534ce02b
Francisco Gregorio de las Heras <francisco.gregoriodelasheras@gmail.com>
2021-07-27 14:50:34 +0200

    feat: add logout button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor(public router: Router, public snackBar: MatSnackBar) {}
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   ngOnInit(): void {}
+
+  // Log out
+  logout(): void {
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
+    this.snackBar.open('You have successfully logged out.', 'OK', {
+      duration: 3000,
+    });
+    this.router.navigate(['']);
+  }
 }
